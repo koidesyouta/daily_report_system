db36ce7e89bf95e25643b2fddff2aa7a986f8580
shouta.koide <koide@valuedesign.jp>
2024-04-25 10:04:20 +0900

    従業員indexアクションの作成

--- src/main/java/constants/AttributeConst.java
+++ src/main/java/constants/AttributeConst.java
@@ -4,7 +4,7 @@ package constants;
 
 public enum AttributeConst {
 
-    FLUSHa("flush"),
+    FLUSH("flush"),
 
     MAX_ROW("maxlow"),
     PAGE("page"),
--- src/main/java/utils/DBUtil.java
+++ src/main/java/utils/DBUtil.java
@@ -10,14 +10,17 @@ public class DBUtil {
 
     private static EntityManagerFactory emf;
 
+    //EntityManagerインスタンスを生成
     public static EntityManager createEntityManager() {
         return _getEntityManagerFactory().createEntityManager();
     }
-    public static EntityManagerFactory _getEntityManagerFactory() {
-        if(emf == null) {
+
+    //EntityManagerFactoryインスタンスを生成
+    private static EntityManagerFactory _getEntityManagerFactory() {
+        if (emf == null) {
             emf = Persistence.createEntityManagerFactory(JpaConst.PERSISTENCE_UNIT_NAME);
         }
+
         return emf;
     }
-
 }
